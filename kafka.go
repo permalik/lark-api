bfa4b66400a7f0de2fac2198ce3e0287d4bfa133
permalik <permalik@protonmail.com>
2025-07-30 21:51:08 -0500

    feat: update group name

--- kafka.go
+++ kafka.go
@@ -15,7 +15,7 @@ func InitProducer() {
 	var err error
 	client, err = kgo.NewClient(
 		kgo.SeedBrokers(seeds...),
-		kgo.ConsumerGroup("llm"),
+		kgo.ConsumerGroup("lark-llm"),
 		kgo.ConsumeTopics("prompt.raw"),
 	)
 	if err != nil {
