ffecf5e6d92e4fbf482038b73f845ce185d48e53
permalik <permalik@protonmail.com>
2025-08-19 19:24:10 -0500

    feat: truncate logs

--- start.sh
+++ start.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+TRUNCATE=$1
+
 if [ -z "$SHELLED" ]; then
     export SHELLED=1
     exec "$SHELL" "$0" "$@"
 fi
 
-go run .
+if [ "$TRUNCATE" = "y" ]; then
+    go run . 2>&1 | sed 's/\x1b\[[0-9;]*m//g' > logs/out.log
+elif [ "$TRUNCATE" = "n" ]; then
+    go run . 2>&1 | sed 's/\x1b\[[0-9;]*m//g' >> logs/out.log
+else
+    echo "Usage: ./start.sh <y|n> (truncate logs)"
+    exit 1
+fi
