01bebcc3b02890e54467701a45541104de447c8f
permalik <permalik@protonmail.com>
2025-08-31 17:12:55 -0500

    operation: update start script

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-TRUNCATE=$1
-
 if [ -z "$SHELLED" ]; then
     export SHELLED=1
     exec "$SHELL" "$0" "$@"
@@ -22,13 +20,5 @@ if [ ! -f "go.sum" ] || [ ! -d "vendor" ]; then
     go mod tidy
 fi
 
-if [ "$TRUNCATE" = "y" ]; then
-    echo "Starting lark-api.."
-    go run . 2>&1 | sed 's/\x1b\[[0-9;]*m//g' > logs/out.log
-elif [ "$TRUNCATE" = "n" ]; then
-    echo "Starting lark-api.."
-    go run . 2>&1 | sed 's/\x1b\[[0-9;]*m//g' >> logs/out.log
-else
-    echo "Usage: ./start.sh <y|n> (truncate logs)"
-    exit 1
-fi
+echo "Starting lark-api.."
+nohup go run . > /dev/null 2>&1 &
